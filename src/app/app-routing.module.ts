f29bfff6d59e92529d6748ee367667dcf6c7f801
Golden <goldenprieto@gmail.com>
2021-03-17 23:37:57 -0500

    Creacion de Modulos y routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NopagesfoundComponent } from './pages/nopagesfound/nopagesfound.component';
-import { PagesComponent } from './pages/pages.component';
+
+//Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
+
+
+
 
 const routes: Routes = [
-
-  { path:'',
-    component:PagesComponent,
-    children:[
-      { path:'bashboard', component: DashboardComponent },
-      { path:'grafica1', component: Grafica1Component }, 
-      { path:'progress', component: ProgressComponent },
-      { path:'', redirectTo:'/bashboard',pathMatch:'full'},
-    ]
-   },
   
-    { path:'register', component: RegisterComponent },
-    { path:'login', component: LoginComponent },
+    // path:'/dashboard' PagesRoutingModule
+    // path:'/auth' AuthRoutingModule
+    { path:'', redirectTo:'/dashboard',pathMatch:'full'},
     { path:'**', component: NopagesfoundComponent }   
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
